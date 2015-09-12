8261d0701f13c0c7bdd7623212a144df3877f19c
Phan Cao Vinh <caovinh04@yahoo.com>
2015-09-12 15:40:08 +0800

    fix Controller filterAction

--- src/WebPlatform/AseagleBundle/Controller/MainController.php
+++ src/WebPlatform/AseagleBundle/Controller/MainController.php
@@ -108,7 +108,7 @@ class MainController extends Controller
         $country_id = $request->query->get('country');
 
         $products = $this->getDoctrine()->getRepository('AseagleBundle:Product')->createQueryBuilder('p')
-            ->where('p.category_id >= :lft and p.category_id <= :rgt '.($country_id != "" ? " and p.place_of_origin = ".$country_id : "").($search_string != "" ? " and p.title LIKE '%".$search_string."%'" : "").($filter_string != "" ? " and ".$filter_string : "" ))
+            ->where('p.category_id >= :lft and p.category_id <= :rgt '.($country_id != "0" && $country_id != "" ? " and p.place_of_origin = ".$country_id : "").($search_string != "" ? " and p.title LIKE '%".$search_string."%'" : "").($filter_string != "" ? " and ".$filter_string : "" ))
             ->setParameter('lft', $category->getLft())
             ->setParameter('rgt', $category->getRgt())
             ->getQuery()
